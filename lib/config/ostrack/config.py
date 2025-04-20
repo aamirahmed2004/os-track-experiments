d5656f7bc04791f9bf91ea1e64ae3fe8bf63c3d9
aamirahmed2004 <syedaamir099@gmail.com>
2025-04-20 14:33:56 -0700

    Modified config

--- lib/config/ostrack/config.py
+++ lib/config/ostrack/config.py
@@ -21,8 +21,9 @@ cfg.MODEL.BACKBONE.SEP_SEG = False
 cfg.MODEL.BACKBONE.CAT_MODE = 'direct'
 cfg.MODEL.BACKBONE.MERGE_LAYER = 0
 cfg.MODEL.BACKBONE.ADD_CLS_TOKEN = False
-cfg.MODEL.BACKBONE.GNN_LAYERS_PER_STAGE = 1 # THIS IS THE NEW PARAMETER
+cfg.MODEL.BACKBONE.GNN_LAYERS_PER_STAGE = 0 # THIS IS THE NEW PARAMETER
 cfg.MODEL.BACKBONE.GNN_TYPE = "GCN" # THIS IS THE NEW PARAMETER
+cfg.MODEL.BACKBONE.CE_TEMPLATE_RANGE = 'CTR_POINT'  # choose between ALL, CTR_POINT, CTR_REC, GT_BOX
 cfg.MODEL.HEAD = edict()
 cfg.MODEL.HEAD.TYPE = "CORNER"  # CORNER, CENTER
 cfg.MODEL.HEAD.NUM_CHANNELS = 256
@@ -31,7 +32,7 @@ cfg.MODEL.HEAD.NUM_CHANNELS = 256
 cfg.TRAIN = edict()
 cfg.TRAIN.LR = 0.0001
 cfg.TRAIN.WEIGHT_DECAY = 0.0001
-cfg.TRAIN.EPOCH = 500
+cfg.TRAIN.EPOCH = 2
 cfg.TRAIN.LR_DROP_EPOCH = 400
 cfg.TRAIN.BATCH_SIZE = 128
 cfg.TRAIN.NUM_WORKER = 8
@@ -41,10 +42,16 @@ cfg.TRAIN.GIOU_WEIGHT = 2.0
 cfg.TRAIN.L1_WEIGHT = 5.0
 cfg.TRAIN.FREEZE_LAYERS = [0, ]
 cfg.TRAIN.PRINT_INTERVAL = 50
-cfg.TRAIN.VAL_EPOCH_INTERVAL = 20
+cfg.TRAIN.VAL_EPOCH_INTERVAL = 2 # UPDATED
 cfg.TRAIN.GRAD_CLIP_NORM = 0.1
 cfg.TRAIN.AMP = False
 cfg.TRAIN.DROP_PATH_RATE = 0.1
+cfg.TRAIN.CE_START_EPOCH = 20
+cfg.TRAIN.CE_WARM_EPOCH = 80
+cfg.TRAIN.SCHEDULER = edict()
+cfg.TRAIN.SCHEDULER.TYPE = "step"
+cfg.TRAIN.SCHEDULER.DECAY_RATE = 0.1
+
 
 # DATA
 cfg.DATA = edict()
@@ -62,11 +69,15 @@ cfg.DATA.SEARCH = edict()
 cfg.DATA.SEARCH.SIZE = 288
 cfg.DATA.SEARCH.FACTOR = 5.0
 cfg.DATA.SEARCH.NUMBER = 1
+cfg.DATA.SEARCH.CENTER_JITTER = 3
+cfg.DATA.SEARCH.SCALE_JITTER = 0.25
 # DATA.TEMPLATE
 cfg.DATA.TEMPLATE = edict()
 cfg.DATA.TEMPLATE.SIZE = 128
 cfg.DATA.TEMPLATE.FACTOR = 2.0
 cfg.DATA.TEMPLATE.NUMBER = 1
+cfg.DATA.TEMPLATE.CENTER_JITTER = 0
+cfg.DATA.TEMPLATE.SCALE_JITTER = 0
 # DATA.VAL
 cfg.DATA.VAL = edict()
 cfg.DATA.VAL.DATASETS_NAME = ["GOT10K_votval"]
@@ -77,6 +88,7 @@ cfg.DATA.TEST = edict()
 cfg.DATA.TEST.DATASETS_NAME = ["VOT20"]
 cfg.DATA.TEST.DATASETS_RATIO = [1]
 cfg.DATA.TEST.SAMPLE_PER_EPOCH = 5000
+
 # DATA.SAMPLER
 cfg.DATA.SAMPLER = edict()
 cfg.DATA.SAMPLER.JOINT = edict()
@@ -102,6 +114,13 @@ cfg.TRACK.LR = 0.4
 cfg.TRACK.UPDATE_TEMPLATE = True
 cfg.TRACK.TEMPLATE_FACTOR = 2.0
 cfg.TRACK.TEMPLATE_SIZE = 128
+
+cfg.TEST = edict()
+cfg.TEST.EPOCH = 8 # UPDATED
+cfg.TEST.SEARCH_FACTOR = 4.0
+cfg.TEST.SEARCH_SIZE = 256
+cfg.TEST.TEMPLATE_FACTOR = 2.0
+cfg.TEST.TEMPLATE_SIZE = 128
 
 def _edict2dict(dest_dict, src_edict):
     if isinstance(dest_dict, dict) and isinstance(src_edict, dict):
@@ -131,7 +150,8 @@ def _update_config(base_cfg, exp_cfg):
                 else:
                     _update_config(base_cfg[k], v)
             else:
-                raise ValueError("{} not exist in config.py".format(k))
+                # raise ValueError("{} not exist in config.py".format(k))
+                print(f"[WARNING] '{k}' not found in config.py — skipping")
     else:
         return
 
